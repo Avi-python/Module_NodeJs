23dff02eab8cd0f5ed26226fda0403752287ea90
Staravi <dadninele@gmail.com>
2023-09-17 00:31:47 +0800

    up to 15

--- 15_Promise.js
+++ 15_Promise.js
@@ -1,20 +1,20 @@
 // 1. 自己撰寫
-const { readFile } = require('fs');
+// const { readFile } = require('fs');
 
-function getFile(path){
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, content) => {
-            if(err) 
-            {
-                reject(err);
-            }
-            else
-            {
-                resolve(content);
-            }
-        });
-    });
-}
+// function getFile(path){
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, content) => {
+//             if(err) 
+//             {
+//                 reject(err);
+//             }
+//             else
+//             {
+//                 resolve(content);
+//             }
+//         });
+//     });
+// }
 
 // getFile("./content/first.txt")
 //     .then((content) => { console.log(content); return getFile("./content/second.txt"); })
@@ -23,13 +23,32 @@ function getFile(path){
 
 // 2. promiseify
 
-const util = require('util');
-const readFilePromise = util.promisify(readFile);
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+
+// const start = async () => {
+//     try
+//     {
+//         let content = await readFilePromise("./content/first.txt", "utf-8");
+//         console.log(content);
+//     }
+//     catch(err)
+//     {
+//         console.log(err);
+//     }
+// }
+
+// start();
+
+// 3. require().promises
+
+const { readFile } = require('fs').promises;
+
 
 const start = async () => {
     try
     {
-        let content = await readFilePromise("./content/first.txt", "utf-8");
+        let content = await readFile("./content/first.txt", "utf-8");
         console.log(content);
     }
     catch(err)
@@ -39,9 +58,8 @@ const start = async () => {
 }
 
 start();
-
-// 3. require().promises
-
+start();
+console.log("hi");
 
 
 
